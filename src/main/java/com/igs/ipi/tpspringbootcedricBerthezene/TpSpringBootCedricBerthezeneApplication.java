9111c4e650f5166030e9a1db0ec35f01a2afe2f3
cedric <cedric.berthezene@gmail.com>
2017-12-14 15:47:55 +0100

    test

---
+++ src/main/java/com/igs/ipi/tpspringbootcedricBerthezene/TpSpringBootCedricBerthezeneApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringbootcedricBerthezene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringBootCedricBerthezeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringBootCedricBerthezeneApplication.class, args);
+	}
+}
