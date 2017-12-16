689a1fad898937604356780412352f783873c73c
cedric <cedric.berthezene@gmail.com>
2017-12-16 14:41:53 +0100

    Identifier le tour du joueur

--- src/main/java/com/igs/ipi/tpspringbootcedricBerthezene/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringbootcedricBerthezene/controller/GameController.java
@@ -1,12 +1,16 @@
 package com.igs.ipi.tpspringbootcedricBerthezene.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.igs.ipi.tpspringbootcedricBerthezene.component.PartieEnCours;
 import com.igs.ipi.tpspringbootcedricBerthezene.model.GameModel;
 import com.igs.ipi.tpspringbootcedricBerthezene.service.GameService;
 
@@ -15,15 +19,47 @@ import com.igs.ipi.tpspringbootcedricBerthezene.service.GameService;
 public class GameController {
 	
 	@Autowired
-	private GameService gameService; 
+	private GameService gameService;
+	
+	@Autowired
+	private PartieEnCours partieEnCours;
 	
 	@GetMapping("/new")
 	public ModelAndView newGame() {
 		GameModel gm = gameService.newGame();
+		partieEnCours.setPartieEnCours(gm);
 		ModelAndView mv = new ModelAndView("game");
 		mv.addObject("gm", gm);  
 		return mv; 
-		
 	}
+	
+	@GetMapping("/")
+	public ModelAndView partieEnCours() {
+		GameModel pec = partieEnCours.getPartieEnCours(); 
+		ModelAndView mv = new ModelAndView("game");
+		mv.addObject("gm", pec);
+		if (pec==null) {
+			throw new RuntimeException("Pas de partie en cours"); 
+		}
+		return mv; 
+	}
+	
+	
+	// Fonction qui retourne la colonne du tableau 
+	@GetMapping("/drop/{numColonne}")
+	public ModelAndView drop(@PathVariable Integer numColonne) {
+		// On sélectionne le template à afficher
+		ModelAndView mv = new ModelAndView("game"); 
+		// Je récupère la partie en cours
+		GameModel gm = partieEnCours.getPartieEnCours(); 
+		//
+		int index = numColonne - 1; 
+		// Appel de la fonction add - ajout d'un nouveau jeton en fonction de l'indice 
+		gm.add(index); 
+		// Ajout de la variable à la vue
+		mv.addObject("gm", gm); 
+		return mv; 
+	}	
+	
 	
 }
--- src/main/java/com/igs/ipi/tpspringbootcedricBerthezene/model/GameModel.java
+++ src/main/java/com/igs/ipi/tpspringbootcedricBerthezene/model/GameModel.java
@@ -1,11 +1,23 @@
 package com.igs.ipi.tpspringbootcedricBerthezene.model;
 
+import enu.Jeton;
+
 public class GameModel {
 	
 	private String nom1; 
 	private String nom2;
 	private int jetons[][] = new int[7][6]; 
+	// Booléen pour définir le tour du joueur
+	private boolean tourJoueur1; 
 	
+	public boolean isTourJoueur1() {
+		return tourJoueur1;
+	}
+
+	public void setTourJoueur1(boolean tourJoueur1) {
+		this.tourJoueur1 = tourJoueur1;
+	}
+
 	public GameModel(String nom1, String nom2) {
 		this.nom1 = nom1; 
 		this.nom2 = nom2; 
@@ -17,6 +29,8 @@ public class GameModel {
 				jetons[i][j] = 0; 
 			}
 		}
+		// Pour chaque nouvelle partie, c'est toujours au premier joueur de commencer
+		tourJoueur1 = true; 
 	}
 	
 	public String getNom1() {
@@ -33,8 +47,30 @@ public class GameModel {
 	} 
 	
 	// Tableau de int
-	public int[][] getTabInt() {
-		return this.tableauInt; 
+	public int[][] getJetons() {
+		return this.jetons; 
+	}
+	
+	// Ajout d'un nouveau jeton au tableau 
+	public void add(Integer index) {
+		// On parcours le tableau de bas en haut en itérant sur la ligne
+		for (int h = this.jetons.length - 1; h >= 0; h--) {
+			// On vérifie si la ligne sélectionné possède déjà un jeton avec 0
+			if (jetons[h][index] == 0) {
+				if (tourJoueur1) {
+					jetons[h][index] = 1;
+					tourJoueur1 = false;
+				} else {
+					// le joueur 2 a joué donc tourJoueur1 revient à true
+					jetons[h][index] = 2; 
+					tourJoueur1 = true; 
+				}
+				
+				return; // Ce return permet de ne faire l'opération qu'une fois 
+			}
+		}
+		// La place est déjà occupé erreur
+		throw new IllegalArgumentException("Place déjà occupée par le jeton"); 
 	}
 	
 	
